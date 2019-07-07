bb86fc6bf16c4ad538433abdddd9d95237cc53fd
Thiago do Nascimento <thiago@desenvolvedor.tech>
2019-07-07 20:58:52 +0100

    Starting

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,8 @@
+'''
+File for deploying with mod_wsgi on Apache
+'''
 import sys
 
-sys.path.insert(0, "/home/admin/web/chatpi.desenvolvedor.tech/public_html/")
+sys.path.insert(0, "absolute/path/to/your/application/folder")
 
 from chat import app as application
